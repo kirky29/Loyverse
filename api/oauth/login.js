c6c999d7a1d2c77952e80f99d6d463d426ce56bf
kirky29 <kbolam@me.com>
2025-08-13 16:27:38 +0100

    Fix OAuth API functions and Vercel config for single-account design

--- api/oauth/login.js
+++ api/oauth/login.js
@@ -3,16 +3,9 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  const { account } = req.query;
-  const accountNum = Number(account || '1');
-  
-  if (![1, 2].includes(accountNum)) {
-    return res.status(400).send('Invalid account. Use account=1 or account=2');
-  }
-
   const clientId = process.env.LOYVERSE_CLIENT_ID;
-  const redirectUri = process.env.LOYVERSE_REDIRECT_URI || `https://${req.headers.host}/api/oauth/callback`;
-  const scope = process.env.LOYVERSE_SCOPE || 'receipts.read stores.read';
+  const redirectUri = `https://${req.headers.host}/api/oauth/callback`;
+  const scope = 'receipts.read stores.read';
 
   if (!clientId) {
     return res.status(500).send('Missing LOYVERSE_CLIENT_ID env var');
@@ -23,7 +16,6 @@ export default async function handler(req, res) {
   authorizeUrl.searchParams.set('client_id', clientId);
   authorizeUrl.searchParams.set('redirect_uri', redirectUri);
   authorizeUrl.searchParams.set('scope', scope);
-  authorizeUrl.searchParams.set('state', String(accountNum));
 
   return res.redirect(authorizeUrl.toString());
 }
